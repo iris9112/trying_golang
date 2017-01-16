eef0363c79b68334f8a0ccdf53909f47fa8ae124
Isabel Cristina Ruiz Buritica <iris9112@gmail.com>
2017-01-15 19:12:18 -0500

    Adicionar nuevas practicas

---
+++ Practicas/input.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+    var s string
+    fmt.Scanf("%q", &s)
+    fmt.Println(s)
+    return
+}
--- Practicas/interfaces.go
+++ Practicas/interfaces.go
@@ -1,24 +1,60 @@
-//interfaces
+// Interfaces
+
+// Declarar una interface llamada speaker con un método llamado speak.
+// Declarar un struct llamado ingles que represente a una persona que hable Inglés.
+// Declarar un struct llamado chino que represente a una persona que hable chino.
+// Implementar la interface speaker para cada struct usando un valor y strings: "Hello World" y "你好世界"
+// Declarar una variable del tipo speaker y asignarle una dirección de tipo Inglés y llamar el método.
+// Hay que hacerlo para chino.
 
 package main
 
 import "fmt"
 
-//Speaker implementa lo que dice una persona
+// speaker implementa lo que dice una persona
 type speaker interface {
 	speak()
 }
 
-//Ingles representa una persona que habla Ingles
+// inglés representa a una persona que habla inglés
+
 type ingles struct{}
 
-//speak implementa la interface Speaker
+// speak implementa la interface speaker
 func (ingles) speak() {
-	fmt.Println("Hello world")
+	fmt.Println("Hello World")
 }
+
+// chino representa a una persona que habla chino
 
 type chino struct{}
 
+// speak implementa la interface speaker
 func (chino) speak() {
-	fmt.Println("Hello world")
+	fmt.Println("你好世界")
+}
+
+func main() {
+	//Declarar una variable para interface
+	var sp speaker
+
+	// Asignarle un valor a la interface y vamos a llamar al método (de la interface)
+	var i ingles
+	sp = i
+	sp.speak()
+
+	// Chino
+	var c chino
+	sp = c
+	sp.speak()
+
+	// Crear nuevos valores y llamar la función
+	decirHola(new(ingles))
+	decirHola(&chino{})
+}
+
+// decirHola abstrae la función de hablar
+
+func decirHola(sp speaker) {
+	sp.speak()
 }
